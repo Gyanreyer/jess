4faa72997f82e35028364ef6c09c0206782d435a
Ryan Geyer <ryan@waymark.com>
2020-06-29 22:32:19 -0400

    Fill out homepage admin config

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,20 @@
 import Head from "next/head";
 import { Component } from "react";
-import { attributes, react as HomeContent } from "../content/home.md";
+import homeContent from "../content/home.md";
 
 export default class Home extends Component {
   render() {
-    let { title, cats } = attributes;
+    const { pageTitle, reel, about, contact } = homeContent.attributes;
+
     return (
       <>
         <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+          <title>{pageTitle}</title>
         </Head>
         <article>
-          <h1>{title}</h1>
-          <HomeContent />
-          <ul>
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
-          </ul>
+          <video src={reel} />
+          <h1>About</h1>
+          {about}
         </article>
       </>
     );
