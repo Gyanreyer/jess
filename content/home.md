27065f26cb00e2d10007574c911c136011356bdc
Ryan Geyer <ryan@waymark.com>
2020-06-29 22:48:20 -0400

    Set up homepage content

--- content/home.md
+++
@@ -1,7 +0,0 @@
----
-pageTitle: Home
-reel: /media/conner_omalley_beckoning.mp4
-about: Hello! I'm Jess.
-contact: Please contact me.
----
-
--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,21 @@ import homeContent from "../content/home.md";
 
 export default class Home extends Component {
   render() {
-    const { pageTitle, reel, about, contact } = homeContent.attributes;
+    // const { heading: aboutHeading } = aboutSectionContent.attributes;
+    // const AboutContent = aboutSectionContent.react;
 
-    return (
-      <>
-        <Head>
-          <title>{pageTitle}</title>
-        </Head>
-        <article>
-          <video src={reel} />
-          <h1>About</h1>
-          {about}
-        </article>
-      </>
-    );
+    return <div>Hello</div>;
+    // (
+    //   <>
+    //     <Head>
+    //       <title>Home</title>
+    //     </Head>
+    //     <article>
+    //       {/* <video src={reel} muted autoPlay loop /> */}
+    //       <h1>{aboutHeading}</h1>
+    //       <AboutContent />
+    //     </article>
+    //   </>
+    // );
   }
 }
