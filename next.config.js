0b37bd1ff3db3209d402588fdfb79552f0492efd
Ryan Geyer <ryan@waymark.com>
2020-07-06 14:41:31 -0400

    Add fonts

--- next.config.js
+++ next.config.js
@@ -2,14 +2,19 @@ const withPlugins = require("next-compose-plugins");
 /* Plugins */
 // Optimizes image assets at build time
 const optimizedImages = require("next-optimized-images");
+// Enables using fonts
+const withFonts = require("next-fonts");
 
-module.exports = withPlugins([optimizedImages], {
-  webpack: (cfg) => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      loader: "frontmatter-markdown-loader",
-      options: { mode: ["react-component"] },
-    });
-    return cfg;
-  },
-});
+module.exports = withPlugins(
+  [optimizedImages],
+  withFonts({
+    webpack: (cfg) => {
+      cfg.module.rules.push({
+        test: /\.md$/,
+        loader: "frontmatter-markdown-loader",
+        options: { mode: ["react-component"] },
+      });
+      return cfg;
+    },
+  })
+);
--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,40 @@ export default function App({ Component, pageProps }) {
     <>
       <Component {...pageProps} />
       <style jsx global>{`
+        /* Normal font weight */
+        @font-face {
+          font-family: "Avenir";
+          src: url("/fonts/Avenir-Book.ttf");
+          font-weight: normal;
+          font-style: normal;
+        }
+        @font-face {
+          font-family: "Avenir";
+          src: url("/fonts/Avenir-BookOblique.ttf");
+          font-weight: normal;
+          font-style: oblique;
+        }
+
+        /* Bold font weight */
+        @font-face {
+          font-family: "Avenir";
+          src: url("/fonts/Avenir-Heavy.ttf");
+          font-weight: bold;
+          font-style: normal;
+        }
+        @font-face {
+          font-family: "Avenir";
+          src: url("/fonts/Avenir-HeavyOblique.ttf");
+          font-weight: bold;
+          font-style: oblique;
+        }
+
         html,
         body {
           margin: 0;
           padding: 0;
 
-          font-family: sans-serif;
+          font-family: "Avenir", sans-serif;
           color: #2e2e2e;
         }
       `}</style>
