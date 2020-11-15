fe141d80b99eb467dc04af4b8a1c8927347f2dbc
Ryan Geyer <ryan@waymark.com>
2020-11-15 15:45:29 -0500

    Build out motion work page

---
+++ components/work/workPageHeading.js
@@ -0,0 +1,32 @@
+import { blueAccentColor, orangeAccentColor } from "../../constants/colors";
+
+export default function WorkPageHeading({ title, subtext }) {
+  return (
+    <div className="heading-box">
+      <h1>{title}</h1>
+      <p>{subtext}</p>
+      <style jsx>{`
+        .heading-box {
+          border: 0.5rem solid ${blueAccentColor};
+          margin: 1rem 4% 3rem;
+          padding: 2rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+
+          h1 {
+            color: ${orangeAccentColor};
+            font-size: 3rem;
+            margin: 0 0 1rem;
+          }
+
+          p {
+            max-width: 40rem;
+            margin: 0;
+          }
+        }
+      `}</style>
+    </div>
+  );
+}
--- pages/work/motion.js
+++ pages/work/motion.js
@@ -1,15 +1,48 @@
 import HoverVideoPlayer from "react-hover-video-player";
 import ReactMarkdown from "react-markdown";
+import { NextSeo } from "next-seo";
 
 import Layout from "../../components/shared/layout";
 import LazyImage from "../../components/shared/lazyImage";
+import WorkPageHeading from "../../components/work/workPageHeading";
+import { blueAccentColor, backgroundColor } from "../../constants/colors";
+
 import motionContents from "../../content/work/motion.yml";
 
-export default function MotionPage() {
-  const { projects } = motionContents;
+const { projects, seo } = motionContents;
 
+export default function MotionPage() {
   return (
     <Layout theme="light" pageTitle={motionContents.pageTitle}>
+      <NextSeo
+        title={seo.pageTitle}
+        description={seo.description}
+        openGraph={{
+          type: "website",
+          title: seo.pageTitle,
+          description: seo.description,
+          images: seo.ogImage
+            ? [
+                {
+                  url: require(`../../public${seo.ogImage}?resize&size=1200`),
+                  width: 1200,
+                },
+                {
+                  url: require(`../../public${seo.ogImage}?resize&size=400`),
+                  width: 400,
+                },
+                {
+                  url: require(`../../public${seo.ogImage}?resize&size=100`),
+                  width: 100,
+                },
+              ]
+            : [],
+        }}
+      />
+      <WorkPageHeading
+        title={motionContents.pageTitle}
+        subtext={motionContents.pageDescription}
+      />
       <ul>
         {projects.map(({ title, description, thumbnail, video }) => (
           <li key={title}>
@@ -20,12 +53,20 @@ export default function MotionPage() {
                   className="background-image"
                   placeholderSrc={require(`../../public${thumbnail}?resize&size=24`)}
                   src={require(`../../public${thumbnail}`)}
-                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                  shouldCoverContainer
                 />
               }
+              className="hover-video-player"
+              sizingMode="container"
+              preload="metadata"
             />
-            <div className="description">
-              <ReactMarkdown source={description} />
+            <div className="overlay">
+              <div className="overlay-border">
+                <div className="overlay-contents">
+                  <h3>{title}</h3>
+                  <ReactMarkdown source={description} />
+                </div>
+              </div>
             </div>
           </li>
         ))}
@@ -34,26 +75,42 @@ export default function MotionPage() {
         ul {
           list-style-type: none;
           margin: 0;
-          padding: 0;
+          padding: 0 4%;
 
           display: grid;
           grid-template-columns: repeat(2, 1fr);
+          grid-gap: 3rem 1.5rem;
 
           li {
             position: relative;
 
-            .description {
+            :global(.hover-video-player) {
+              padding-top: 100%;
+            }
+
+            &:nth-child(3n) {
+              grid-column: 1 / 3;
+
+              :global(.hover-video-player) {
+                padding-top: 56.25%;
+              }
+            }
+
+            :global(.background-image) {
+              pointer-events: none;
+            }
+
+            .overlay {
               position: absolute;
               top: 0;
               left: 0;
               width: 100%;
               height: 100%;
+              padding: 1.5rem 1rem;
+              box-sizing: border-box;
               z-index: 1;
-              background-color: rgba(53, 53, 53, 0.5);
+              background-color: ${blueAccentColor}99;
               color: white;
-              display: flex;
-              justify-content: center;
-              align-items: center;
 
               pointer-events: none;
 
@@ -61,8 +118,32 @@ export default function MotionPage() {
               transition: opacity 0.4s;
             }
 
-            &:hover .description {
+            &:hover .overlay {
               opacity: 1;
+            }
+
+            .overlay-border {
+              border: 0.5rem solid ${backgroundColor};
+              width: 100%;
+              height: 100%;
+              padding: 1rem;
+              box-sizing: border-box;
+            }
+
+            .overlay-contents {
+              position: relative;
+              top: 50%;
+              height: 50%;
+
+              h3 {
+                margin: 0;
+                font-weight: normal;
+                font-size: 2rem;
+              }
+
+              :global(p) {
+                margin: 1.2rem 0;
+              }
             }
           }
         }
