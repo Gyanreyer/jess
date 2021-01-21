a8b8a3c5d1e3ad89b4b525509c68cdbb08f4aa00
Ryan Geyer <ryan@waymark.com>
2021-01-20 20:41:03 -0500

    Disable ImageGallery lazyload

--- components/work/imageContent.js
+++ components/work/imageContent.js
@@ -33,9 +33,6 @@ function ImageGalleryContent({ contentConfig: { imageFiles, columnWidth } }) {
             imageFile.endsWith(".gif") ? "" : "?nf_resize=fit&w=1080&h=1080"
           }`,
         }))}
-        renderItem={({ original }) => (
-          <Image className="image-gallery-image" src={original} />
-        )}
         showFullscreenButton={false}
         showThumbnails={false}
         showPlayButton={false}
