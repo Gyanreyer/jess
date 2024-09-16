6fd9d7bef40ff2e3e63325b54eb63d3dc706842a
Ryan <gyanreyer@gmail.com>
2024-09-16 11:06:24 -0400

    Improve img-sequence smoothness on slow connections

--- scripts/prep-img-sequence-files.mjs
+++ scripts/prep-img-sequence-files.mjs
@@ -2,23 +2,23 @@ import { readdir } from "node:fs/promises";
 import sharp from "sharp";
 
 const path = import.meta
-  .resolve("./phone-img-sequence")
+  .resolve("../src/assets/img/work/crate-and-barrel/phone-img-sequence")
   .slice("file://".length);
 
 const fileNames = await readdir(path);
 
-const prefixLength = "CB_Phone_".length;
+// const prefixLength = "CB_Phone_".length;
 
 fileNames.sort((a, b) => {
-  const aNumber = Number(a.slice(prefixLength));
-  const bNumber = Number(b.slice(prefixLength));
+  const aNumber = Number(a);
+  const bNumber = Number(b);
   return aNumber - bNumber;
 });
 
 for (let i = 0; i < fileNames.length; i++) {
   const filePath = `${path}/${fileNames[i]}`;
   sharp(filePath)
-    .resize(720)
-    .toFormat("webp")
+    .resize(450)
+    .webp({ quality: 80, preset: "picture", effort: 6 })
     .toFile(`${String(i).padStart(3, "0")}.webp`);
 }
--- src/_components/shared/img-sequence.js
+++ src/_components/shared/img-sequence.js
@@ -1,120 +1,135 @@
-class ImgSequence extends HTMLElement {
-  connectedCallback() {
-    const sourceTags = this.getElementsByTagName("source");
+{
+  class ImgSequence extends HTMLElement {
+    connectedCallback() {
+      const sourceTags = this.getElementsByTagName("source");
 
-    const sourceCount = sourceTags.length;
-    const sources = new Array(sourceCount);
+      const sourceCount = sourceTags.length;
 
-    for (let i = 0; i < sourceCount; ++i) {
-      sources[i] = sourceTags[i].src;
-    }
+      /**
+       * @type {Array<HTMLImageElement>}
+       */
+      const images = new Array(sourceCount);
 
-    const canvas = document.createElement("canvas");
-    const dataCanvasWidth = this.dataset.imgWidth;
-    const dataCanvasHeight = this.dataset.imgHeight;
-    if (dataCanvasWidth) {
-      canvas.width = Number(dataCanvasWidth);
-    }
-    if (dataCanvasHeight) {
-      canvas.height = Number(dataCanvasHeight);
-    }
-
-    const ctx = canvas.getContext("2d");
-    if (!ctx) {
-      return;
-    }
-
-    this.querySelector("img")?.replaceWith(canvas);
-
-    let currentImageIndex = 0;
-
-    /**
-     * @type {Array<HTMLImageElement>}
-     */
-    const images = new Array(sourceCount);
-
-    const dataScrollRangeStart = this.dataset.scrollRangeStart;
-    let scrollRangeStart = 0.2;
-    /**
-     * @type {(() => void) | undefined}
-     */
-    let recalculateScrollRangeStart;
-    if (dataScrollRangeStart === "auto") {
-      recalculateScrollRangeStart = () => {
-        const boundingRect = this.getBoundingClientRect();
-        const absoluteYPos =
-          window.innerHeight -
-          (boundingRect.top + boundingRect.height / 2 + window.scrollY);
-
-        scrollRangeStart = Math.max(absoluteYPos / window.innerHeight, 0);
-      };
-    } else if (dataScrollRangeStart) {
-      scrollRangeStart = Number(dataScrollRangeStart);
-    }
-
-    const dataScrollRangeEnd = this.dataset.scrollRangeEnd;
-    const scrollRangeEnd = dataScrollRangeEnd
-      ? Number(dataScrollRangeEnd)
-      : 0.8;
-
-    const updateImage = async () => {
-      const boundingRect = this.getBoundingClientRect();
-      const yPos =
-        (window.innerHeight - (boundingRect.top + boundingRect.height / 2)) /
-        window.innerHeight;
-
-      const sourceIndex = Math.max(
-        Math.min(
-          Math.floor(
-            ((yPos - scrollRangeStart) / (scrollRangeEnd - scrollRangeStart)) *
-              sourceCount
-          ),
-          sourceCount - 1
-        ),
-        0
-      );
-
-      currentImageIndex = sourceIndex;
-
-      // Start preloading frames surrounding the current one
-      for (
-        let i = Math.max(sourceIndex - 8, 0);
-        i <= Math.min(sourceIndex + 8, sourceCount - 1);
-        ++i
-      ) {
-        if (!images[i]) {
-          images[i] = new Image();
-          images[i].src = sources[i];
-        }
+      for (let i = 0; i < sourceCount; ++i) {
+        const img = new Image();
+        img.fetchPriority = "low";
+        img.src = sourceTags[i].src;
+        images[i] = img;
       }
 
-      const img = images[sourceIndex];
+      const canvas = document.createElement("canvas");
+      const dataCanvasWidth = this.dataset.imgWidth;
+      const dataCanvasHeight = this.dataset.imgHeight;
+      if (dataCanvasWidth) {
+        canvas.width = Number(dataCanvasWidth);
+      }
+      if (dataCanvasHeight) {
+        canvas.height = Number(dataCanvasHeight);
+      }
 
-      if (!img.complete) {
-        // Ensure the image is decoded before we proceed to draw it
-        await img.decode();
-        // If the current image has changed since we started loading this one, don't draw
-        if (currentImageIndex !== sourceIndex) {
+      const ctx = canvas.getContext("2d");
+      if (!ctx) {
+        return;
+      }
+
+      this.querySelector("img")?.replaceWith(canvas);
+
+      /**
+       * @type {number | null}
+       */
+      let currentImageIndex = null;
+
+      const dataScrollRangeStart = this.dataset.scrollRangeStart;
+      let scrollRangeStart = 0.2;
+      /**
+       * @type {(() => void) | undefined}
+       */
+      let recalculateScrollRangeStart;
+      if (dataScrollRangeStart === "auto") {
+        recalculateScrollRangeStart = () => {
+          const boundingRect = this.getBoundingClientRect();
+          const absoluteYPos =
+            window.innerHeight -
+            (boundingRect.top + boundingRect.height / 2 + window.scrollY);
+
+          scrollRangeStart = Math.max(absoluteYPos / window.innerHeight, 0);
+        };
+      } else if (dataScrollRangeStart) {
+        scrollRangeStart = Number(dataScrollRangeStart);
+      }
+
+      const dataScrollRangeEnd = this.dataset.scrollRangeEnd;
+      const scrollRangeEnd = dataScrollRangeEnd
+        ? Number(dataScrollRangeEnd)
+        : 0.8;
+
+      const updateImage = async () => {
+        const boundingRect = this.getBoundingClientRect();
+        const yPos =
+          (window.innerHeight - (boundingRect.top + boundingRect.height / 2)) /
+          window.innerHeight;
+
+        const sourceIndex = Math.max(
+          Math.min(
+            Math.floor(
+              ((yPos - scrollRangeStart) /
+                (scrollRangeEnd - scrollRangeStart)) *
+                sourceCount
+            ),
+            sourceCount - 1
+          ),
+          0
+        );
+
+        if (sourceIndex === currentImageIndex) {
           return;
         }
-      }
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(img, 0, 0);
-    };
+        if (currentImageIndex !== null) {
+          // Abort loading of previous image
+          const previousImage = images[currentImageIndex];
+          previousImage.dispatchEvent(new Event("abort"));
+          previousImage.fetchPriority = "low";
+        }
 
-    window.addEventListener("scroll", updateImage, {
-      passive: true,
-    });
+        currentImageIndex = sourceIndex;
 
-    const recalculateImageSequencePosition = () => {
-      recalculateScrollRangeStart?.();
-      updateImage();
-    };
+        const img = images[sourceIndex];
+        img.fetchPriority = "high";
 
-    requestAnimationFrame(recalculateImageSequencePosition);
-    window.addEventListener("resize", recalculateImageSequencePosition);
+        if (img.complete) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.drawImage(img, 0, 0);
+        } else {
+          /** @type {Promise<void>} */ (
+            new Promise((resolve, reject) => {
+              img.onload = resolve;
+              img.onerror = reject;
+              img.onabort = reject;
+            }).then(
+              () => {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.drawImage(img, 0, 0);
+              },
+              () => {}
+            )
+          );
+        }
+      };
+
+      window.addEventListener("scroll", updateImage, {
+        passive: true,
+      });
+
+      const recalculateImageSequencePosition = () => {
+        recalculateScrollRangeStart?.();
+        updateImage();
+      };
+
+      requestAnimationFrame(recalculateImageSequencePosition);
+      window.addEventListener("resize", recalculateImageSequencePosition);
+    }
   }
-}
 
-window.customElements.define("img-sequence", ImgSequence);
+  window.customElements.define("img-sequence", ImgSequence);
+}
