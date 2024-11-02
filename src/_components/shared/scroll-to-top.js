fe4a5f23e17a39fcc99bbc3605dacc3d501ef9d5
Ryan <gyanreyer@gmail.com>
2024-11-01 21:11:32 -0400

    Make scroll-to-top button behavior web component based

---
+++ src/_components/shared/scroll-to-top.js
@@ -0,0 +1,39 @@
+{
+  class ScrollToTop extends HTMLElement {
+    /**
+     * @type {IntersectionObserver | null}
+     */
+    navObserver = null;
+
+    connectedCallback() {
+      this.navObserver = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting) {
+          // If the nav is visible, hide the scroll-to-top button
+          this.classList.remove("visible");
+        } else {
+          // If the nav is not visible, show the scroll-to-top button
+          this.classList.add("visible");
+        }
+      });
+
+      const navElement = document.getElementsByTagName("nav")[0];
+      this.navObserver.observe(navElement);
+
+      const link = this.getElementsByTagName("a")[0];
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      });
+    }
+
+    disconnectedCallback() {
+      // Clean up the IntersectionObserver
+      this.navObserver?.disconnect();
+    }
+  }
+
+  window.customElements.define("scroll-to-top", ScrollToTop);
+}
